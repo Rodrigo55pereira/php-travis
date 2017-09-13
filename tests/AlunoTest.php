6da4772f482d10aef697a5ee49843ecbed431722
rpereirm <rodrigo55pereira@gmail.com>
2017-09-13 20:41:37 -0300

    Atualização

--- tests/AlunoTest.php
+++ tests/AlunoTest.php
@@ -5,27 +5,24 @@ use Application\Models\Aluno;
 
 class AlunoTest extends TestCase
 {
-   //Função de teste de tipo - Compara tipo da variável Matricula
     public function testType()
     {
         $aluno = new Aluno("Jose", "B", 12, 001);
         $this->assertInternalType('int', $aluno->getMatricula());
     }
 
-    //Função de teste de número de matricula - Verifica numero de alunos cadastrados
     public function testNumeroMatricula()
     {
         $aluno = new Aluno("Jose", "B", 12, 001);
         $this->assertEquals(001, $aluno->getMatricula());
     }
-    //Função de teste que verifica turma do aluno
+
     public function testTurma()
     {
         $aluno = new Aluno("Maria", "C", 10, 002);
         $this->assertEquals("C", $aluno->getTurma());
     }
 
-    //Função que compara nota do aluno atual com nota esperada
     public function testNota()
     {
         $aluno = new Aluno("Joao", "C", 10, 002);
